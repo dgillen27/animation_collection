738d369e6603ed0157d0b2ac975836020ab86b35
Daniel Gillen <danielbgillen@gmail.com>
2019-07-02 13:09:24 -0400

    created hover animation for title page

---
+++ src/components/Title.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div className="title">
+      <div class="text-wrapper">
+        <h1>
+          WELCOME!
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default Title;

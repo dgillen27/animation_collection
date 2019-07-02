4d75b8aa247804c86fc8bc963ad2dfa1a02e6deb
Daniel Gillen <danielbgillen@gmail.com>
2019-07-02 14:11:40 -0400

    setup hamburger to x transiton, needs refactoring

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function HamburgerMenu(props) {
+  const { onClick, menuToggle } = props
   return (
-    <div className="hamburger-menu" onClick={props.onClick}>
-      <div className="burger"></div>
-      <div className="burger"></div>
-      <div className="burger"></div>
+    <div className="hamburger-menu" onClick={onClick}>
+      <div className="burger" id={menuToggle? "top-burger" : null}></div>
+      <div className="burger" id={menuToggle? "hidden-burger" : null}></div>
+      <div className="burger" id={menuToggle? "bottom-burger" : null}></div>
     </div>
   )
 }

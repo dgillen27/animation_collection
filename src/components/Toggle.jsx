79fbb64aca47cd1ab90731d84544baf4f4b09074
Daniel Gillen <danielbgillen@gmail.com>
2019-07-03 17:47:52 -0400

    fixed bug with slide deck, changed background color for toggle

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Toggle(props) {
   const { handleCheckboxChange, checked } = props;
   return (
-    <div className="toggle-page" style={{ background: checked? "#030303" : "white"}}>
+    <div className="toggle-page" style={{ background: checked? "#131313" : "white"}}>
       <input id="toggle" name="toggle" type="checkbox" onClick={handleCheckboxChange}/>
       <label id="toggle-label" htmlFor="toggle">
       </label>
